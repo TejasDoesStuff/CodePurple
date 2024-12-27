04eab230e94e2e7a382e0ae9c04ff8bdc911254e
datboi_212 <datboi212mc@gmail.com>
2024-12-26 19:51:26 -0800

    started to work on about us page

--- codepurplewebsite/src/components/AboutUs.jsx
+++ codepurplewebsite/src/components/AboutUs.jsx
@@ -1,10 +1,16 @@
 function AboutUs() {
-    return (
-      <div className="h-[50vh] w-screen bg-codePurple z-100 mt-[-1px] flex flex-row items-center z-20">
+  return (
+    <>
+      <div className="h-[50vh] w-screen bg-codePurple z-100 mt-[-1px] flex flex-row items-center justify-center">
+        <div className="font-kode font-bold">
+          <h1 className="text-white text-9xl text-center"> About Us </h1>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center">
         
       </div>
-    );
-  }
-  
-  export default AboutUs;
-  
+    </>
+  );
+}
+
+export default AboutUs;
