18e2e8bd8a382e4d41e33ed392840edd32913938
datboi_212 <datboi212mc@gmail.com>
2025-01-09 22:07:22 -0800

    goofy

--- codepurplewebsite/src/pages/Contributors.jsx
+++ codepurplewebsite/src/pages/Contributors.jsx
@@ -1,12 +1,13 @@
 import PersonCard from "../leadership-components/PersonCard";
 import Omkar from "../assets/leadership/Omkar.jpg";
+import Tejas from "../assets/5827_logo.jpg";
 
 function Contributors(){
     return(
         <div className="h-screen w-screen bg-white flex flex-col items-center justify-center">
             <h1 className="text-9xl font-kode m-10 text-codePurple gradient-scroll">Contributors</h1>
             <div className="flex flex-row">
-                <PersonCard name="Tejas Panja" title="Contributor" description="" />
+                <PersonCard name="Tejas Panja" title="Contributor" description="" image={Tejas}/>
                 <PersonCard name="Omkar Page" title="Contributor" description="" image={Omkar}/>
             </div>
         </div>
