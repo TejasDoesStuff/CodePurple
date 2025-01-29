a93068d923bfa30c2f33893291d48bd75f2103e2
datboi_212 <datboi212mc@gmail.com>
2025-01-28 22:15:57 -0800

    add links to contributors

--- codepurplewebsite/src/pages/Contributors.jsx
+++ codepurplewebsite/src/pages/Contributors.jsx
@@ -7,8 +7,12 @@ function Contributors(){
         <div className="h-screen w-screen bg-white flex flex-col items-center justify-center">
             <h1 className="text-9xl font-kode font-bold m-10 text-codePurple gradient-scroll">Contributors</h1>
             <div className="flex flex-row">
-                <PersonCard name="Tejas Panja" title="Lead Web Dev" description="" image={Tejas}/>
-                <PersonCard name="Omkar Page" title="Code Lead" description="" image={Omkar}/>
+                <a href="https://github.com/TejasDoesStuff">
+                    <PersonCard name="Tejas Panja" title="Lead Web Dev" description="" image={Tejas}/>
+                </a>
+                <a href="https://github.com/datboi-212">
+                    <PersonCard name="Omkar Page" title="Code Lead" description="" image={Omkar}/>
+                </a>
             </div>
         </div>
     )
